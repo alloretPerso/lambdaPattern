f620cda892cdf8dadd9736bb23e2b93230503bd8
alexandre.lloret <alloret.perso@gmail.com>
2020-11-03 21:31:20 +0000

    Defence programming

--- java/src/module1/Consumer.java
+++ java/src/module1/Consumer.java
@@ -1,10 +1,13 @@
 package module1;
 
+import java.util.Objects;
+
 @FunctionalInterface
 public interface Consumer<T> {
     void accept(T t);
 
     default Consumer<T> andThen(Consumer<T> other) {
+        Objects.requireNonNull(other);
         return (T t) -> {
             this.accept(t);
             other.accept(t);
