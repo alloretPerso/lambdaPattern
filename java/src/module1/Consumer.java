4ff7ebba0b07d54742cf2531bd70fc1008c77f09
alexandre.lloret <alloret.perso@gmail.com>
2020-11-03 21:20:57 +0000

    Basic groupping lambda

---
+++ java/src/module1/Consumer.java
@@ -0,0 +1,6 @@
+package module1;
+
+@FunctionalInterface
+public interface Consumer<T>{
+    void accept(T t);
+}
--- java/src/module1/main.java
+++ java/src/module1/main.java
@@ -2,6 +2,16 @@ package module1;
 
 public class main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        example1();
+    }
+
+    private static void example1() {
+        Consumer<String> c1 = s -> System.out.println("c1 = "+ s);
+        Consumer<String> c2 = s -> System.out.println("c2 = "+ s);
+        Consumer<String> c3 = s -> {
+            c1.accept(s);
+            c2.accept(s);
+        };
+        c3.accept("\"Hello World\"");
     }
 }
